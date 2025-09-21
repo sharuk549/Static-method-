a5dc4caeec5402c880d0c358e1d5fe1adad6dd7b
Sharukeshwaran V <sharukeshwaranvthf@gmail.com>
2025-09-21 19:13:18 +0530

    Add files via upload

---
+++ CalculatorTest.java
@@ -0,0 +1,12 @@
+import mypackage.Calculator;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
